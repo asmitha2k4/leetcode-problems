104165054bf15ecd787a3260338d68627f187871
Asmitha <asmithaarul21@gmail.com>
2026-03-01 16:34:01 +0530

    Create Next Permutation.java

---
+++ java/Next Permutation.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    public void nextPermutation(int[] nums) {
+        
+        int n = nums.length;
+        int i = n - 2;
+        
+        // Step 1: Find first decreasing element from right
+        while(i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        
+        // Step 2: If found, find just bigger element from right
+        if(i >= 0) {
+            int j = n - 1;
+            
+            while(nums[j] <= nums[i]) {
+                j--;
+            }
+            
+            swap(nums, i, j);
+        }
+        
+        // Step 3: Reverse right side
+        reverse(nums, i + 1, n - 1);
+    }
+    
+    
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+    
+    
+    private void reverse(int[] nums, int start, int end) {
+        while(start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
