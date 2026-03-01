7313d38e85eafe9df55c26b7d14b0d0a0380060c
Asmitha <asmithaarul21@gmail.com>
2026-03-01 17:29:56 +0530

    Create  Same Tree.java

---
+++ java/Same Tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // If both are null, they are the same
+        if (p == null && q == null) return true;
+        
+        // If one is null but not the other, they are different
+        if (p == null || q == null) return false;
+        
+        // Check current node values and recurse on left and right
+        return (p.val == q.val) 
+            && isSameTree(p.left, q.left) 
+            && isSameTree(p.right, q.right);
+    }
+}
