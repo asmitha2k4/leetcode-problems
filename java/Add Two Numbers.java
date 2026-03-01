fefc982452967f87951ec57fbf9c1dc3dea3d659
Asmitha <asmithaarul21@gmail.com>
2026-03-01 15:32:07 +0530

    Implement function to add two numbers represented by linked lists

---
+++ java/Add Two Numbers.java
@@ -0,0 +1,36 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        
+        int carry = 0;
+        
+        while(l1 != null || l2 != null) {
+            
+            int sum = carry;
+            
+            if(l1 != null) {
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
+            
+            if(l2 != null) {
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
+            
+            carry = sum / 10;
+            
+            ListNode newNode = new ListNode(sum % 10);
+            current.next = newNode;
+            current = current.next;
+        }
+        
+        if(carry > 0) {
+            current.next = new ListNode(carry);
+        }
+        
+        return dummy.next;
+    }
+}
