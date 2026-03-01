d1f2025bd41b7c42aabc8099d5ed37142e919cb2
Asmitha <asmithaarul21@gmail.com>
2026-03-01 16:12:48 +0530

    Create  Generate Parentheses.java

---
+++ java/Generate Parentheses.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    
+    public List<String> generateParenthesis(int n) {
+        
+        List<String> result = new ArrayList<>();
+        
+        generate(result, "", 0, 0, n);
+        
+        return result;
+    }
+    
+    public void generate(List<String> result, String current,
+                         int open, int close, int n) {
+        
+        if(current.length() == n * 2) {
+            result.add(current);
+            return;
+        }
+        
+        if(open < n) {
+            generate(result, current + "(", open + 1, close, n);
+        }
+        
+        if(close < open) {
+            generate(result, current + ")", open, close + 1, n);
+        }
+    }
+}
