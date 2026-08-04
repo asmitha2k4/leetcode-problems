106cb446806b2af4cf77e475732310707db03179
Asmitha <asmithaarul21@gmail.com>
2026-08-04 18:59:52 +0530

    Add solution for surrounded regions problem
    Implement algorithm to capture surrounded regions in a board.

---
+++ java/Surrounded Regions/Solution.java
@@ -0,0 +1,64 @@
+class Solution {
+
+    public void solve(char[][] board) {
+
+        if (board == null || board.length == 0) {
+            return;
+        }
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // First and Last Column
+        for (int i = 0; i < rows; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+
+            if (board[i][cols - 1] == 'O') {
+                dfs(board, i, cols - 1);
+            }
+        }
+
+        // First and Last Row
+        for (int j = 0; j < cols; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
+            }
+
+            if (board[rows - 1][j] == 'O') {
+                dfs(board, rows - 1, j);
+            }
+        }
+
+        // Convert remaining O -> X and # -> O
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int row, int col) {
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        if (row < 0 || row >= rows || col < 0 || col >= cols || board[row][col] != 'O') {
+            return;
+        }
+
+        board[row][col] = '#';
+
+        dfs(board, row + 1, col);
+        dfs(board, row - 1, col);
+        dfs(board, row, col + 1);
+        dfs(board, row, col - 1);
+    }
+}
